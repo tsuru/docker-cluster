d9a3c8363a33de219a0a47a69036d2f027a4d545
Francisco Souza <f@souza.cc>
2013-06-07 16:23:22 -0300

    container: added InspectContainer method
    This first version is very dumb: at the moment, the cluster does not
    store any information about containers, so it just ask all nodes about
    the given containers, returning any error that is not ErrNoSuchContainer
    as the result.
    
    This isn't the ideal solution, obviously.

--- cluster/container.go
+++ cluster/container.go
@@ -6,6 +6,8 @@ package cluster
 
 import (
 	"github.com/dotcloud/docker"
+	dcli "github.com/fsouza/go-dockerclient"
+	"sync"
 )
 
 // CreateContainer creates a container in one of the nodes.
@@ -17,4 +19,39 @@ func (c *Cluster) CreateContainer(config *docker.Config) (string, *docker.Contai
 	node := c.next()
 	container, err := node.CreateContainer(config)
 	return node.id, container, err
+}
+
+// InspectContainer returns information about a container by its ID, getting
+// the information from the right node.
+func (c *Cluster) InspectContainer(id string) (*docker.Container, error) {
+	c.mut.RLock()
+	defer c.mut.RUnlock()
+	var wg sync.WaitGroup
+	finish := make(chan int8, 1)
+	errChan := make(chan error, len(c.nodes))
+	result := make(chan *docker.Container, 1)
+	for _, n := range c.nodes {
+		wg.Add(1)
+		go func(n node) {
+			defer wg.Done()
+			container, err := n.InspectContainer(id)
+			if err == nil {
+				result <- container
+			} else if err != dcli.ErrNoSuchContainer {
+				errChan <- err
+			}
+		}(n)
+	}
+	go func() {
+		wg.Wait()
+		close(finish)
+	}()
+	select {
+	case container := <-result:
+		return container, nil
+	case err := <-errChan:
+		return nil, err
+	case <-finish:
+		return nil, dcli.ErrNoSuchContainer
+	}
 }
--- cluster/container_test.go
+++ cluster/container_test.go
@@ -6,6 +6,7 @@ package cluster
 
 import (
 	"github.com/dotcloud/docker"
+	dclient "github.com/fsouza/go-dockerclient"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -43,5 +44,86 @@ func TestCreateContainer(t *testing.T) {
 	}
 	if container.ID != "e90302" {
 		t.Errorf("CreateContainer: wrong container ID. Want %q. Got %q.", "e90302", container.ID)
+	}
+}
+
+func TestInspectContainer(t *testing.T) {
+	body := `{"Id":"e90302","Path":"date","Args":[]}`
+	server1 := httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "No such container", http.StatusNotFound)
+	}))
+	defer server1.Close()
+	server2 := httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(body))
+	}))
+	defer server2.Close()
+	server3 := httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "No such container", http.StatusNotFound)
+	}))
+	defer server3.Close()
+	cluster, err := New(
+		Node{ID: "handler0", Address: server1.URL},
+		Node{ID: "handler1", Address: server2.URL},
+		Node{ID: "handler2", Address: server3.URL},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	id := "e90302"
+	container, err := cluster.InspectContainer(id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if container.ID != id {
+		t.Errorf("InspectContainer(%q): Wrong ID. Want %q. Got %q.", id, id, container.ID)
+	}
+	if container.Path != "date" {
+		t.Errorf("InspectContainer(%q): Wrong Path. Want %q. Got %q.", id, "date", container.Path)
+	}
+}
+
+func TestInspectContainerNoSuchContainer(t *testing.T) {
+	server1 := httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "No such container", http.StatusNotFound)
+	}))
+	defer server1.Close()
+	server2 := httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "No such container", http.StatusNotFound)
+	}))
+	defer server2.Close()
+	cluster, err := New(
+		Node{ID: "handler0", Address: server1.URL},
+		Node{ID: "handler1", Address: server2.URL},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	id := "e90302"
+	container, err := cluster.InspectContainer(id)
+	if container != nil {
+		t.Errorf("InspectContainer(%q): Expected <nil> container, got %#v.", id, container)
+	}
+	if err != dclient.ErrNoSuchContainer {
+		t.Errorf("InspectContainer(%q): Wrong error. Want %#v. Got %#v.", id, dclient.ErrNoSuchContainer, err)
+	}
+}
+
+func TestInspectContainerFailure(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "No such container", http.StatusInternalServerError)
+	}))
+	defer server.Close()
+	cluster, err := New(Node{ID: "handler0", Address: server.URL})
+	if err != nil {
+		t.Fatal(err)
+	}
+	id := "a2033"
+	container, err := cluster.InspectContainer(id)
+	if container != nil {
+		t.Errorf("InspectContainer(%q): Expected <nil> container, got %#v.", id, container)
+	}
+	if err == nil {
+		t.Errorf("InspectContainer(%q): Expected non-nil error, got <nil>", id)
 	}
 }
