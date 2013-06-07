bf381fdfe5e1c07a5c06d1f420108c9e4d73a71e
Francisco Souza <f@souza.cc>
2013-06-07 15:42:42 -0300

    container: change CreateContainer to also return the ID of the node

--- cluster/container.go
+++ cluster/container.go
@@ -8,6 +8,13 @@ import (
 	"github.com/dotcloud/docker"
 )
 
-func (c *Cluster) CreateContainer(config *docker.Config) (*docker.Container, error) {
-	return c.next().CreateContainer(config)
+// CreateContainer creates a container in one of the nodes.
+//
+// It returns the ID of the node where the container is running, and the
+// container, or an error, in case of failures. It will always return the ID of
+// the node, even in case of failures.
+func (c *Cluster) CreateContainer(config *docker.Config) (string, *docker.Container, error) {
+	node := c.next()
+	container, err := node.CreateContainer(config)
+	return node.id, container, err
 }
