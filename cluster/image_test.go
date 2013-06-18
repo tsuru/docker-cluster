6699f081c27671a8b7cc6570e66574ee0e812580
Francisco Souza <f@souza.cc>
2013-06-18 16:55:00 -0300

    cluster: add test to make sure that RemoveImage returns proper error

--- cluster/image_test.go
+++ cluster/image_test.go
@@ -5,6 +5,7 @@
 package cluster
 
 import (
+	"github.com/fsouza/go-dockerclient"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -35,5 +36,28 @@ func TestRemoveImage(t *testing.T) {
 	}
 	if !called {
 		t.Errorf("RemoveImage(%q): Did not call node HTTP server", name)
+	}
+}
+
+func TestRemoveImageNotFound(t *testing.T) {
+	server1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "No such image", http.StatusNotFound)
+	}))
+	defer server1.Close()
+	server2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "No such image", http.StatusNotFound)
+	}))
+	defer server2.Close()
+	cluster, err := New(
+		Node{ID: "handler0", Address: server1.URL},
+		Node{ID: "handler1", Address: server2.URL},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	name := "tsuru/python"
+	err = cluster.RemoveImage(name)
+	if err != docker.ErrNoSuchImage {
+		t.Errorf("RemoveImage(%q): wrong error. Want %#v. Got %#v.", docker.ErrNoSuchImage, err)
 	}
 }
