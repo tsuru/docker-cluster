060c2a753f84786483bb69992c7f1309c12e2056
Francisco Souza <fss@corp.globo.com>
2014-01-23 18:39:00 -0200

    cluster: fix year in copyright banner

--- cluster/scheduler.go
+++ cluster/scheduler.go
@@ -1,4 +1,4 @@
-// Copyright 2013 docker-cluster authors. All rights reserved.
+// Copyright 2014 docker-cluster authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
